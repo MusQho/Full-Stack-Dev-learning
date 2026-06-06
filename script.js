72ed42db4153a382c2848ed1f23683074c448b41
MusQho <muhammaddhuhaqolby@gmail.com>
2026-06-06 22:00:10 +0700

    mastering conditionals

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const developerName = "MUH. DHUHA QOLBY TAQIYYA";
 const daysStudied = 26;
-const welcomeMessage = "Halo, saya " + developerName + " dan saya sudah belajar selama " + daysStudied + " hari!"
+const welcomeMessage = "Halo, saya " + developerName + " dan saya sudah belajar selama " + daysStudied + " hari!";
 alert(welcomeMessage);
+
+if (daysStudied >= 30) {
+    alert("Luar biasa! Kamu sudah belajar lebih dari sebulan!");
+} else if (daysStudied > 20) {
+    alert("Keren, kamu sudah konsisten belajar lebih dari 20 hari!");
+} else {
+    alert("Semangat! Terus lanjutkan belajarmu setiap hari!");
+}
