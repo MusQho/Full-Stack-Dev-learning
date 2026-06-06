69c8e037d67d4edab4d37fe6746bd3f6060d097c
MusQho <muhammaddhuhaqolby@gmail.com>
2026-06-06 22:12:51 +0700

    mastering functions

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const developerName = "MUH. DHUHA QOLBY TAQIYYA";
 const daysStudied = 26;
-const welcomeMessage = "Halo, saya " + developerName + " dan saya sudah belajar selama " + daysStudied + " hari!";
-alert(welcomeMessage);
 
-if (daysStudied >= 30) {
-    alert("Luar biasa! Kamu sudah belajar lebih dari sebulan!");
-} else if (daysStudied > 20) {
-    alert("Keren, kamu sudah konsisten belajar lebih dari 20 hari!");
-} else {
-    alert("Semangat! Terus lanjutkan belajarmu setiap hari!");
+function createStatuMessage(hari) {
+    if (hari >= 30) {
+        return("Luar biasa! Kamu sudah belajar lebih dari sebulan!");
+    } else if (hari > 20) {
+        return("Keren, kamu sudah konsisten belajar lebih dari 20 hari!");
+    } else {
+        return("Semangat! Terus lanjutkan belajarmu setiap hari!");
+    }
 }
+
+alert(createStatuMessage(daysStudied));
+alert(createStatuMessage(10));
